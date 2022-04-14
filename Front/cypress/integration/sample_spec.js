b61e5afb70e27f3ecc6ca43ba1f51edde647066b
KirihimeNatsuki <enzo.lemarchand@ynov.com>
2022-04-14 19:46:27 +0200

    Push 14/04

--- Front/cypress/integration/sample_spec.js
+++ Front/cypress/integration/sample_spec.js
@@ -11,7 +11,7 @@ describe("add Product", () => {
         cy.get('input')
             .type('{selectall}{backspace}{1}');
         cy.get('button').contains("Ajouter au panier").click(); 
-        cy.wait(10000)
+        cy.wait(15000)
         cy.get('p').then(($p) => {
             if ($p.text().includes('Enregistré dans le panier')) {
             } else {
@@ -20,3 +20,22 @@ describe("add Product", () => {
           })
     });
 });
+
+// describe("remove Product from cart", () => {
+//     it("Test removeProduct", () => {
+//         cy.visit("http://localhost:3000/");
+//         cy.wait(10000);
+//         cy.get('div').contains('Aller sur panier').click();
+//         cy.wait(10000);
+//         const count = cy.get('button').contains('Supprimer du panier').its('length')
+//         cy.log(Object.keys(count));
+//         cy.get('button').contains('Supprimer du panier').then(($button) => {
+//             const items = $button.toArray()
+//             return Cypress._.sample(items)
+//         })
+//         .click();
+//         cy.wait(15000)
+//         cy.get('p').contains('Produit bien supprimé').should('exist');
+//         cy.get('button').contains('Supprimer du panier').its('length').should('equal', count - 1)
+//     });
+// });
