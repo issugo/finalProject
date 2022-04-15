ec545b681c673d1ebcb3305e4625a9c6e76dcdc2
maurin <maurin.margail@ynov.com>
2022-04-15 11:52:17 +0200

    delete non necessary cypress test

--- Front/cypress/integration/sample_spec.js
+++ Front/cypress/integration/sample_spec.js
@@ -1,25 +1,25 @@
-describe("add Product", () => {
-    it("Test addProduct", () => {
-        cy.visit("http://localhost:3000/");
-        cy.wait(10000);
-        cy.get('img').should('have.length', 20)
-            .then(($img) => {
-                const items = $img.toArray()
-                return Cypress._.sample(items)
-            })
-            .click();
-        cy.get('input')
-            .type('{selectall}{backspace}{1}');
-        cy.get('button').contains("Ajouter au panier").click(); 
-        cy.wait(15000)
-        cy.get('p').then(($p) => {
-            if ($p.text().includes('Enregistré dans le panier')) {
-            } else {
-                cy.contains('Trop de quantité').should('exist');
-            }
-          })
-    });
-});
+// describe("add Product", () => {
+//     it("Test addProduct", () => {
+//         cy.visit("http://localhost:3000/");
+//         cy.wait(10000);
+//         cy.get('img').should('have.length', 20)
+//             .then(($img) => {
+//                 const items = $img.toArray()
+//                 return Cypress._.sample(items)
+//             })
+//             .click();
+//         cy.get('input')
+//             .type('{selectall}{backspace}{1}');
+//         cy.get('button').contains("Ajouter au panier").click(); 
+//         cy.wait(15000)
+//         cy.get('p').then(($p) => {
+//             if ($p.text().includes('Enregistré dans le panier')) {
+//             } else {
+//                 cy.contains('Trop de quantité').should('exist');
+//             }
+//           })
+//     });
+// });
 
 // describe("remove Product from cart", () => {
 //     it("Test removeProduct", () => {
